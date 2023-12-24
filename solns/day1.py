8ba839f1d57c6f97b4fcf3664dd86714163c9eb4
Marvin <mdtensuan_dev@protonmail.com>
2023-12-24 13:32:43 +0800

    Day 1 solutions

---
+++ solns/day1.py
@@ -0,0 +1,43 @@
+import re
+
+
+def part1():
+    with open("./inputs/day1.txt") as f:
+        data = f.read().splitlines()
+
+    digits = [[c for c in line if c.isdigit()] for line in data]
+    all_digits = [int(f"{item[0]}{item[-1]}") for item in digits]
+    print(f"{sum(all_digits)=}")
+
+
+def part2():
+    with open("./inputs/day1.txt") as f:
+        data = f.read().splitlines()
+
+    word_value = {
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9,
+    }
+
+    pattern = "|".join([*word_value.keys(), *(str(i) for i in word_value.values())])
+    pattern_lookahead = f"(?=({pattern}))"
+
+    clean_data: list[list[str]] = [re.findall(pattern_lookahead, line) for line in data]
+
+    def numerify(character: str) -> int:
+        if character.isdigit():
+            return int(character)
+        return word_value[character]
+
+    all_digits = [
+        numerify(f"{item[0]}") * 10 + numerify(f"{item[-1]}") for item in clean_data
+    ]
+
+    print(f"{sum(all_digits)=}")
